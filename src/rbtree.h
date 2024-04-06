e2600cfda6a08da950d044ec0fed7fef306fdc90
youngmanpark <py980627@gmail.com>
2024-04-06 05:54:36 +0000

    rb트리 헤더파일 주석

--- src/rbtree.h
+++ src/rbtree.h
@@ -3,30 +3,39 @@
 
 #include <stddef.h>
 
-typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
+// tree를 구현하기 위한 구조체와 열거형을 정의
+typedef enum
+{
+  RBTREE_RED,
+  RBTREE_BLACK
+} color_t;
 
 typedef int key_t;
 
-typedef struct node_t {
+// tree의 각 노드를 표현하는 구조체
+typedef struct node_t
+{
   color_t color;
   key_t key;
   struct node_t *parent, *left, *right;
 } node_t;
 
-typedef struct {
+// tree 자체를 나타내는 구조체
+typedef struct
+{
   node_t *root;
-  node_t *nil;  // for sentinel
+  node_t *nil; // for sentinel
 } rbtree;
 
-rbtree *new_rbtree(void);
-void delete_rbtree(rbtree *);
+rbtree *new_rbtree(void);     // 새 트리를 생성하는 함수
+void delete_rbtree(rbtree *); // 트리를 순회하면서 각 노드의 메모리를 반환하는 함수
 
-node_t *rbtree_insert(rbtree *, const key_t);
-node_t *rbtree_find(const rbtree *, const key_t);
-node_t *rbtree_min(const rbtree *);
-node_t *rbtree_max(const rbtree *);
-int rbtree_erase(rbtree *, node_t *);
+node_t *rbtree_insert(rbtree *, const key_t);     // 노드를 삽입하고 불균형을 복구하는 함수
+node_t *rbtree_find(const rbtree *, const key_t); // key에 해당하는 노드를 반환하는 함수
+node_t *rbtree_min(const rbtree *);               // key가 최소값에 해당하는 노드를 반환하는 함수
+node_t *rbtree_max(const rbtree *);               // key가 최대값에 해당하는 노드를 반환하는 함수
+int rbtree_erase(rbtree *, node_t *);             // 노드를 삭제하는 함수
 
-int rbtree_to_array(const rbtree *, key_t *, const size_t);
+int rbtree_to_array(const rbtree *, key_t *, const size_t); //'t'를 inorder로 'n'번 순회한 결과를 'arr'에 담는 함수
 
-#endif  // _RBTREE_H_
+#endif // _RBTREE_H_
